7db11e0b1853293cbfee8c2c1948eb79c78b8b82
hassaanali <ahassaan575@gmail.com>
2024-10-16 20:14:01 +0100

    update

--- components/ImageUpload.js
+++ components/ImageUpload.js
@@ -9,9 +9,9 @@ export const uploadImage = (img, cb) => {
     const data = new FormData();
     data.append("file", img);
     data.append("upload_preset", "easy-transport");
-    data.append("cloud_name", "arslanvirk");
+    data.append("cloud_name", "ddt8dl0ty");
     infoNotification("Wait", "Please wait, image is uploading");
-    return fetch("https://api.cloudinary.com/v1_1/arslanvirk/image/upload", {
+    return fetch("https://api.cloudinary.com/v1_1/ddt8dl0ty/image/upload", {
       method: "post",
       body: data,
     })
